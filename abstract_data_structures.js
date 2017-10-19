799bfe25f67bad79051ecd4aa3d1c8d1f1980db8
Rizky <co.terbang@gmail.com>
2017-10-20 01:22:34 +0700

    added sort and binary search in buckets, still not working as intended, use the normal one first

--- abstract_data_structures.js
+++ abstract_data_structures.js
@@ -4,6 +4,9 @@
 // ------------
 // 2. Implement
 
+// const Benchmark = require('benchmark')
+// var suite = new Benchmark.Suite;
+
 class Node
 {
   //untuk menyimpan nilai
@@ -51,7 +54,6 @@ class Bucket
 
   get(node)
   {
-    debugger
     for (var i = 0; i < this.isi.length; i++)
     {
       if (node.getWord() === this.isi[i].getWord())
@@ -104,37 +106,121 @@ class Hash
 
   get(node)
   {
-    debugger
-    for (var i = 0; i < this.buckets.length; i++)
-    {
-      if (node.getIndex() === this.buckets[i].getValue())
+    // pencarian dengan menggunakan cek satu per satu
+    // for (var i = 0; i < this.buckets.length; i++)
+    // {
+    //   if (node.getIndex() === this.buckets[i].getValue())
+    //   {
+    //     return this.buckets[i].get(node);
+    //   }
+    // }
+    // return 0;
+
+      let start = 0;
+      let end = this.buckets.length - 1;
+      let mid = Math.floor((start + end) / 2);
+      while (true)
       {
-        return this.buckets[i].get(node);
+        debugger
+        if (node.getIndex() === this.buckets[mid].getValue())
+        {
+          return this.buckets[mid].get(node);
+        }
+        else if (node.getIndex() < this.buckets[mid].getValue())
+        {
+          end = mid - 1;
+        }
+        else if (node.getIndex() > this.buckets[mid].getValue())
+        {
+          start = mid + 1;
+        }
+        else
+        {
+            return -1;
+        }
+        mid = Math.floor((start + end) / 2);
+      }
+      return 0;
+  }
+
+  sort()
+  {
+    let temp;
+    for (let i = 0; i < 30; i++)
+    {
+      while (i >= 0 && this.buckets[i].getValue() > this.buckets[i + 1].getValue())
+      {
+        temp = this.buckets[i];
+        this.buckets[i] = this.buckets[i + 1];
+        this.buckets[i + 1] = temp;
+        i--;
       }
     }
-    return 0;
   }
+
+  print(end)
+  {
+    for (var i = 0; i < end; i++)
+    {
+      console.log(this.buckets[i].getValue());
+    }
+  }
+}
+
+// ------------
+// 3. Get real
+
+
+function main() {
+
+  sort_from_file('./wordlist/random_wordlist.txt')
+  // sort_from_file('./wordlist/reversed_wordlist.txt')
+  // sort_from_file('./wordlist/sorted_wordlist.txt')
+  return 0;
+}
+
+function sort_from_file(filename) {
+  let fs = require('fs')
+  let hashObject = new Hash();
+
+  fs.readFile(filename, (err, data) => {
+
+    if (err)
+      return console.log(err)
+
+    let items = data.toString().split("\r\n")
+
+    console.log(filename)
+    console.log("--------")
+    console.log(items.length);
+    // console.log(heapSort(items))
+
+    for (var i = 0; i < data.length; i++)
+    {
+      hashObject.add(new Node(`${items[i]}`));
+    }
+    let word = new Node("undefined");
+    hashObject.sort();
+    hashObject.print(30)
+  })
 
 }
 
+main()
 
 
 
-let word = new Node("AAB");
-let word2 = new Node("AAAA");
-let word3 = new Node("BB");
-let word4 = new Node("C");
-
-let hashtables = new Hash();
-
-hashtables.add(word);
-hashtables.add(word2);
-hashtables.add(word3);
-hashtables.add(word4);
-
-
-let word5 = new Node("Rizky")
-// console.log(hashtables.get(word5));
-console.log(hashtables.get(word3));
-// ------------
-// 3. Get real
+// suite.add('hash', function()
+// {
+//   main()
+// })
+// .on('cycle', function(event)
+// {
+//   console.log(event.target.toString());
+// })
+// .on('complete', function()
+// {
+//   console.log("fastest is " + this.filter('fastest').map('name'));
+// })
+//
+// .run({'async' : true})
